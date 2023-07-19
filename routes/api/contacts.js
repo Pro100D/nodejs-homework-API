d2fd6a5f2018af9cea12fd7dfd66e80e853b211e
Danil Takiylin <danadanil900@gmail.com>
2023-07-19 16:40:44 +0200

    add controllers, decorator

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,115 +1,23 @@
-import HttpError from "../../helpers/httpError.js";
+import contactsMethod from "../../controllers/controllers.js";
 import isValidId from "../../middlevares/isValidObjectId.js";
-import Contact, {
-  addSchema,
-  favoriteSchema,
-} from "../../models/contactShema.js";
 
 import express from "express";
 
 const router = express.Router();
 
-router.get("/", async (req, res, next) => {
-  try {
-    const allContact = await Contact.find();
-    res.json(allContact);
-  } catch (error) {
-    next(error);
-  }
-});
+router.get("/", contactsMethod.getAllContact);
 
-router.get("/:contactId", isValidId, async (req, res, next) => {
-  try {
-    const { contactId } = req.params;
-    const contactById = await Contact.findById(contactId);
+router.get("/:contactId", isValidId, contactsMethod.getContactById);
 
-    if (!contactById) {
-      throw HttpError(404, "Not Faund");
-    }
+router.post("/", contactsMethod.addContact);
 
-    res.json(contactById);
-  } catch (error) {
-    next(error);
-  }
-});
+router.delete("/:contactId", isValidId, contactsMethod.deleteContact);
 
-router.post("/", async (req, res, next) => {
-  try {
-    const { error } = addSchema.validate(req.body);
-    if (error) {
-      throw HttpError(400, error.message);
-    }
+router.put("/:contactId", isValidId, contactsMethod.updateContactById);
 
-    const contactAdd = await Contact.create(req.body);
-    res.status(201).json(contactAdd);
-  } catch (error) {
-    next(error);
-  }
-});
-
-router.delete("/:contactId", isValidId, async (req, res, next) => {
-  try {
-    const { contactId } = req.params;
-    if (!contactId) {
-      throw HttpError(404, "Not Faund");
-    }
-    console.log(contactId);
-
-    await Contact.findOneAndRemove({ _id: contactId });
-
-    res.json("contact deleted");
-  } catch (error) {
-    next(error);
-  }
-});
-
-router.put("/:contactId", isValidId, async (req, res, next) => {
-  try {
-    const { contactId } = req.params;
-    const { error } = addSchema.validate(req.body);
-
-    if (error) {
-      throw HttpError(400, `missing fields ${error.message}`);
-    }
-    const contactUpdate = await Contact.findOneAndUpdate(
-      { _id: contactId },
-      req.body,
-      {
-        new: true,
-      }
-    );
-
-    res.json(contactUpdate);
-    if (!contactUpdate) {
-      throw HttpError(404, "Not Faund");
-    }
-  } catch (error) {
-    next(error);
-  }
-});
-
-router.patch("/:contactId/favorite", isValidId, async (req, res, next) => {
-  try {
-    const { contactId } = req.params;
-    const { error } = favoriteSchema.validate(req.body);
-
-    if (error) {
-      throw HttpError(400, `missing fields ${error.message}`);
-    }
-    const updateStatusContact = await Contact.findOneAndUpdate(
-      { _id: contactId },
-      req.body,
-      {
-        new: true,
-      }
-    );
-
-    res.json(updateStatusContact);
-    if (!updateStatusContact) {
-      throw HttpError(404, "Not Faund");
-    }
-  } catch (error) {
-    next(error);
-  }
-});
+router.patch(
+  "/:contactId/favorite",
+  isValidId,
+  contactsMethod.updateFavoriteOnContact
+);
 export default router;
